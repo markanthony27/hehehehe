6e7b14532c8350a0cda94f7b656ff2d87e42da15
MARK ANTHONY <casabuena.mm12@gmail.com>
2021-09-21 05:54:16 +0000

    MODIFIED

--- Activity2.1.py
+++ Activity2.1.py
@@ -10,22 +10,28 @@ avg = float(sum/4)
 print(" ")
 print("Average Grade is {}.".format((avg)))
 print(" ")
-if avg >= 75:
-    print("You passed!")
-else:
-    print("You Failed!")
 
 if avg >=99 and avg<=100:
     print("Your {} Average is Equivalent to A.".format(avg))
+    print("You Passed!")
 elif avg>=90 and avg<=99:
     print("Your {} Average is Equivalent to B.".format(avg))
+    print("You Passed!")
 elif avg>=80 and avg<=90:
     print("Your {} Average is Equivalent to C.".format(avg))
+    print("You Passed!")
 elif avg>=71 and avg<=80:
-    print("Your {} Average is Equivalent to D.".format(avg))
+    if avg>=75 and avg<=80:
+        print("Your {} Average is Equivalent to D.".format(avg))
+        print("You Passed!")
+    else:
+        print("Your {} Average is Equivalent to D.".format(avg))
+        print("You Failed!")
 elif avg>=61 and avg<=71:
     print("Your {} Average is Equivalent to E.".format(avg))
+    print("You Failed!")
 elif avg>=60:
     print("Your {} Average is Equivalent to F.".format(avg))
+    print("You Failed!")
 else:
     print("Invalid Syntax")
